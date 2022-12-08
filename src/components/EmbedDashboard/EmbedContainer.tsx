66f128509af9285231a024629c8fecec0e1937f9
1950019 <priyansiparida@gmail.com>
2022-12-08 18:09:28 +0530

    fixed height

--- src/components/EmbedDashboard/EmbedContainer.tsx
+++ src/components/EmbedDashboard/EmbedContainer.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const EmbedContainer = styled.div`
   width: 100%;
-  height: 99vh;
+  height: 99.23vh;
   & > iframe {
     width: 100%;
     height: 100%;
